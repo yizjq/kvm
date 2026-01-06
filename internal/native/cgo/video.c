0e697f0e57f9e5b60f7e1ff9d7694941dbb6317a
yizjq <108796401+yizjq@users.noreply.github.com>
2026-01-06 23:57:38 +0800

    Update video.c

--- internal/native/cgo/video.c
+++ internal/native/cgo/video.c
@@ -94,8 +94,8 @@ static void detect_sleep_mode()
 
 double calculate_bitrate(float bitrate_factor, int width, int height)
 {
-    const int32_t base_bitrate_high = 2000;
-    const int32_t base_bitrate_low = 512;
+    const int32_t base_bitrate_high = 50000;
+    const int32_t base_bitrate_low = 5000;
 
     double pixels = (double)width * height;
     double ref_pixels = 1920.0 * 1080.0;
